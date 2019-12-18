fd372e207df9b12b8f2486d25257aa4c48b4a961
Aleff <aleffbruno@gmail.com>
2019-12-18 14:07:53 -0300

    naoSeiSeAsConfigsEstaoCorretasDoReduxESaga start to write reducer/actions/saga

--- src/main/App.js
+++ src/main/App.js
@@ -2,23 +2,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+
+import '../config/ReactotronConfig';
+
+import { Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './Routes';
+
+import history from '../services/history';
+import store from '../store/index';
 // import Logo from '../components/templates/Logo';
 // import Nav from '../components/templates/Nav';
 // import Footer from '../components/templates/Footer';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            {/* <div className="app">
-                <Logo />
-                <Nav />
+        <Provider store={store}>
+            <Router history={history}>
+                {/* <div className="app">
+                    <Logo />
+                    <Nav />
+                    <Routes />
+                    <Footer />
+                </div> */}
                 <Routes />
-                <Footer />
-            </div> */}
-            <Routes />
-        </BrowserRouter>
+            </Router>
+        </Provider>
     )
 }
